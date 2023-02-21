02760614394b7e707eb0fab3b9799f076b02a7e2
NNEUID <nneuid@gmail.com>
2023-02-21 12:12:53 +0000

    creating pages

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ipsa ratione, tempore minima inventore error
+      accusantium nemo deserunt delectus voluptas!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ipsa ratione, tempore minima inventore error
+      accusantium nemo deserunt delectus voluptas!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
