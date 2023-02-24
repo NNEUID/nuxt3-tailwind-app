177e5a11330d18fa0bfb05dba14855bebdcfb885
NNEUID <nneuid@gmail.com>
2023-02-24 21:35:23 +0000

    get id from route params on useRoute()

---
+++ pages/products/[id]/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>Product Detail for {{ id }}</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente nemo maxime corporis error, aperiam repellendus
+      fugit facilis porro mollitia molestias!</p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped></style>
